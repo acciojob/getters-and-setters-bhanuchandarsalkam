01c3ca15e726306dcf3250b383ec5c29f24bb342
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-07-16 21:09:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 //complete this code
-class Person {}
+class Person {
+	name;
+	age;
+	constructor(name,age){
+		this.name=name;
+		this.age=age;
+	}
+	get name(){
+		return this.name;
+	}
+	set age(){
+		return this.age;
+	}
+}
+const p1=new Person('bhanu',21);
 
-class Student extends Person {}
+class Student extends Person {
+	super(name,age);
+	function study(){
+		console.log(this.name+" is studying");
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	super(name,age);
+	function teach(){
+		console.log(this.name+" is teaching");
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
